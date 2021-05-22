ccad169d8bc2239f5751405e10ab066d32f1c70f
kyohei_i <kyoheishogun.1997@gmail.com>
2021-05-23 05:46:52 +0900

    SecondCommit

--- Kadai11/SelectViewController.swift
+++ Kadai11/SelectViewController.swift
@@ -7,30 +7,35 @@
 
 import UIKit
 
-class SelectViewController: UIViewController {
-    @IBOutlet internal weak var tableView: UITableView!
-    internal let prefectures = [
-        "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県",
-        "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県",
-        "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "滋賀県",
-        "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県",
-        "広島県", "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県",
-        "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
+class SelectViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    static func instantiate() -> SelectViewController? {
+        guard let selectViewController
+                = UIStoryboard.init(name: "Second", bundle: nil)
+                .instantiateInitialViewController()
+                as? SelectViewController else { return nil }
+        return selectViewController
+    }
+    @IBOutlet weak var tableView: UITableView!
+    static var prefecture: String?
+    private let prefectures = [
+        "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県",
+        "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県",
+        "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県",
+        "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
+        "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+        "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
+        "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
+        "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
     ]
-    private(set) var prefectureText: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
         tableView.dataSource = self
+        tableView.delegate = self
     }
-}
-extension SelectViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        prefectureText = prefectures[indexPath.row]
-        performSegue(withIdentifier: "segue", sender: nil)
+    @IBAction private func didTapCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-}
-extension SelectViewController: UITableViewDataSource {
+    // MARK: - tableViewDateSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         prefectures.count
     }
@@ -38,5 +43,10 @@ extension SelectViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = prefectures[indexPath.row]
         return cell
+    }
+    // MARK: - tableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Self.prefecture = prefectures[indexPath.row]
+        dismiss(animated: true, completion: nil)
     }
 }
--- Kadai11/ViewController.swift
+++ Kadai11/ViewController.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet private weak var prefecturesLabel: UILabel!
-
-    @IBAction func cancelExit(segue: UIStoryboardSegue) {
+    var label2: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //label1を作成
+        let label1  = UILabel()
+        label1.frame = CGRect(x: 0, y: 0, width: 90, height: 30)
+        label1.text = "都道府県"
+        label1.textColor = .black
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label1)
+        label1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50).isActive = true
+        label1.topAnchor.constraint(equalTo: view.topAnchor, constant: 90).isActive = true
+        //label2を作成
+        label2 = UILabel()
+        label2.frame = CGRect(x: 0, y: 0, width: 90, height: 30)
+        label2.text = "未選択"
+        label2.textColor = .black
+        label2.textAlignment = .center
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label2)
+        label2.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        label2.topAnchor.constraint(equalTo: view.topAnchor, constant: 90).isActive = true
+        //buttonを作成
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
+        button.setTitle("選択", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(self.segue(_:)), for: UIControl.Event.touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        view.rightAnchor.constraint(equalTo: button.rightAnchor, constant: 50).isActive = true
+        button.topAnchor.constraint(equalTo: view.topAnchor, constant: 85).isActive = true
     }
-    @IBAction func selectPrefecture (segue: UIStoryboardSegue) {
-        guard let selectVC = segue.source as? SelectViewController else { return }
-        prefecturesLabel.text = selectVC.prefectureText
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let prefecture = SelectViewController.prefecture {
+            label2.text = prefecture
+        }
+    }
+    @objc func segue(_ sender: UIButton) {
+        guard let selectViewController = SelectViewController.instantiate() else { return }
+        let navigationController = UINavigationController(rootViewController: selectViewController)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true, completion: nil)
     }
 }
